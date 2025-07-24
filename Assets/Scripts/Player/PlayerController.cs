765e1438cacfffb5acd855e6f495fd0464da0a58
HAKv1N <egor.pestushko@yandex.ru>
2025-07-24 14:50:15 +0300

    Update 1
    1. Импортирован Mirror
    2. Добавлено передвижение
    p.s. все добавления и изменения сразу применяются для мультиплеера

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,65 @@
+using Mirror;
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour
+{
+    [Header("Value")]
+    [SerializeField] private float _speed;
+    [SerializeField] private float _sensitivity;
+
+    [Header("Objects")]
+    [SerializeField] private Transform playerHead;
+
+    private CharacterController characterController;
+    private Transform playerCamera;
+    private Vector3 moveVector;
+    private float rotationX;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>().GetComponent<Transform>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        InitializePlayer();
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        Move();
+        FirstPerson();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        moveVector = transform.forward * v + transform.right * h;
+
+        characterController.Move(moveVector * _speed * Time.deltaTime);
+    }
+
+    private void FirstPerson()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * _sensitivity;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -80, 80);
+
+        characterController.transform.Rotate(0, mouseX, 0);
+        playerHead.localRotation = Quaternion.Euler(rotationX, 0, 0);
+    }
+
+    private void InitializePlayer()
+    {
+        if (isLocalPlayer) return;
+
+        Destroy(playerCamera.gameObject);
+    }
+}
